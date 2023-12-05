5b2969ad6eaff34694dd6677b7433cd8b1ebe0f3
Chamsol Kim <kcsol1005@gmail.com>
2023-12-05 14:49:24 +0900

    add clock

--- js/index.js
+++ js/index.js
@@ -1,11 +1,29 @@
+/* Random */
+
 function randomInt(range) {
   return Math.floor(Math.random() * range);
 }
 
+function randomBackgroundImage() {
+  const images = ["0.jpg", "1.jpg", "2.jpg"];
+  const image = images[randomInt(3)];
+  return `url(img/${image})`;
+}
+
 /* Background */
 
-const images = ["0.jpg", "1.jpg", "2.jpg"];
-function fetchBackgroundImage() {
-  return images[randomInt(3)];
+document.body.style.backgroundImage = randomBackgroundImage();
+
+/* Clock */
+
+function padZero(number) {
+  return String(number).padStart(2, "0");
 }
-document.body.style.backgroundImage = `url(img/${fetchBackgroundImage()})`;
+function currentTimeString() {
+  const date = new Date();
+  const hour = padZero(date.getHours());
+  const minute = padZero(date.getMinutes());
+  return `${hour}:${minute}`;
+}
+const clock = document.getElementById("clock");
+clock.innerText = currentTimeString();
